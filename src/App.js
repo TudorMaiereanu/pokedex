c26d84e4970da849f16692115a3b66751c3ce223
Tudor Maiereanu <tudormaiereanu@gmail.com>
2019-11-07 19:14:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pokemon = [
+    { name: "Bulbasaur" },
+    { name: "Venusaur" },
+    { name: "Ivysaur" },
+  ];
+
+  // This is internal state
+  const [
+    /* getter: */ selectedPokemon, 
+    /* setter: */ setSelectedPokemon,
+  ] = useState(/* initialState */ null);
+
+  console.log("Pokedex rendering...")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pokedex">
+      <div className="pokedex-list">
+        <PokedexList 
+          pokemon={pokemon}
+          selectedPokemon={selectedPokemon}
+          setSelectedPokemon={setSelectedPokemon}
+        />
+      </div>
+      <div className="pokedex-description"></div>
+      <div className="pokedex-image"></div>
+      <div className="pokedex-summary"></div>
     </div>
   );
 }
 
+function PokedexList(props) {
+  const { pokemon, selectedPokemon, setSelectedPokemon } = props;
+
+  return (
+    <ul>
+      {pokemon.map((p, index)=> {
+        const number = String(index + 1).padStart(3, "0");
+        const buttonClass = p.name === selectedPokemon ? "active" : null;
+
+        return (
+          <li key={p.name}>
+            <button 
+              className={buttonClass}
+              onClick={() => 
+              {
+                setSelectedPokemon(p.name);
+                console.log("Pokemon", p.name, "was selected.")
+              }
+            }>
+              <strong>{number}</strong> {p.name}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 export default App;
