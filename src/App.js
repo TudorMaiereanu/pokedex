712460ec8f4cf9a74cb09f8ef4a304b659c28a0f
Tudor Maiereanu <tudormaiereanu@gmail.com>
2019-11-07 20:26:03 +0100

    added description

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const pokemon = [
-    { name: "Bulbasaur" },
-    { name: "Venusaur" },
-    { name: "Ivysaur" },
-  ];
-
-  // This is internal state
-  const [
-    /* getter: */ selectedPokemon, 
-    /* setter: */ setSelectedPokemon,
-  ] = useState(/* initialState */ null);
+  // This is internal state || Array destructuring
+  // [getter, setter] = useState(initialState)
+  const [pokemon, setPokemon] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [details, setDetails] = useState(null);
 
   console.log("Pokedex rendering...")
 
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then(response => response.json())
+      .then(data => {
+        setPokemon(data.results);
+      });
+  }, []);
+
+  useEffect(() => {
+    setDetails(null);
+    if (selectedPokemon != null) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
+        .then(response => response.json())
+        .then(data => {
+          setDetails(data);
+        });
+    }
+  }, [selectedPokemon]);
+  
   return (
     <div className="pokedex">
-      <div className="pokedex-list">
         <PokedexList 
           pokemon={pokemon}
           selectedPokemon={selectedPokemon}
           setSelectedPokemon={setSelectedPokemon}
         />
-      </div>
       <div className="pokedex-description"></div>
-      <div className="pokedex-image"></div>
-      <div className="pokedex-summary"></div>
+      <PokedexImage 
+        details={details}
+      />
+      <PokedexSummary 
+        details={details}
+      />
     </div>
   );
 }
@@ -37,27 +52,62 @@ function PokedexList(props) {
   const { pokemon, selectedPokemon, setSelectedPokemon } = props;
 
   return (
-    <ul>
-      {pokemon.map((p, index)=> {
-        const number = String(index + 1).padStart(3, "0");
-        const buttonClass = p.name === selectedPokemon ? "active" : null;
+    <div className="pokedex-list">
+      <ul>
+        {pokemon.map((p, index)=> {
+          const number = String(index + 1).padStart(3, "0");
+          const buttonClass = p.name === selectedPokemon ? "active" : null;
 
-        return (
-          <li key={p.name}>
-            <button 
-              className={buttonClass}
-              onClick={() => 
-              {
-                setSelectedPokemon(p.name);
-                console.log("Pokemon", p.name, "was selected.")
-              }
-            }>
-              <strong>{number}</strong> {p.name}
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={p.name}>
+              <button 
+                className={buttonClass}
+                onClick={() => 
+                {
+                  setSelectedPokemon(p.name);
+                  console.log("Pokemon", p.name, "was selected.")
+                }
+              }>
+                <strong>{number}</strong> {p.name}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
+
+function PokedexImage(props) {
+  const { details } = props;
+
+  return (
+    <div className="pokedex-image">
+      {details != null && (
+        <img
+          src={details.sprites.front_default}
+          alt={details.name}
+          width="288"
+          height="288"
+        />
+      )}
+    </div>
+  );
+}
+
+function PokedexSummary(props) {
+  const { details } = props;
+
+  return (
+    <div className="pokedex-summary">
+      {details != null && (
+        <div>
+          <h1>{details.name}</h1>
+          <p>NUMBER: {details.id.toString().padStart(3, "0")}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
 export default App;
