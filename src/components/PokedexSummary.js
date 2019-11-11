ea7c28d675e1e186c9ccd159a1bf8f6d2576f7bb
Tudor Maiereanu <tudormaiereanu@gmail.com>
2019-11-11 18:01:44 +0100

    added properties to PokedexSummary

--- src/components/PokedexSummary.js
+++ src/components/PokedexSummary.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import '../App.css';
 
+const collectStats = (stats) => {
+  const statsList = []
+  stats.forEach(stat => {
+    statsList.push({
+      name: stat.stat.name,
+      value: stat.base_stat,
+    });
+  });
+
+  return statsList;
+};
 function PokedexSummary(props) {
   const { details } = props;
+  const statsList = details ? collectStats(details.stats) : [];
   
   return (
     <div className="pokedex-summary">
       {details != null && (
         <>
           <h1>{details.name}</h1>
-          <p>NUMBER: {details.id.toString().padStart(3, "0")}</p>
+          {statsList.map(stat => 
+              <p className="pokedex-property">
+                {`${stat.name}: ${stat.value}`}
+              </p>
+          )}
         </>
       )}
     </div>
