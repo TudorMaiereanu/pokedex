f6a2d9ae96f863083762a3b4ed8879f17c5dbd5f
Tudor Maiereanu <tudormaiereanu@gmail.com>
2019-11-26 21:22:41 +0100

    added skills to moves/skills list

--- src/components/DetailsPokedexComponents/PokedexDetailsPopover.js
+++ src/components/DetailsPokedexComponents/PokedexDetailsPopover.js
@@ -8,11 +8,12 @@ import '../../App.css';
 function PokedexDetailsPopover(props) {
   const { details, popoverClass, setPopoverClass } = props;
   const [selectedMoveObj, setSelectedMoveObj] = useState(null);
+  const [selectedAbilityObj, setSelectedAbilityObj] = useState(null);
   const [ moveDetails, setMoveDetails ] = useState(null);
 
   return (
     <div className={popoverClass}>
-      <PokedexSkillsList details={details} setSelectedMoveObj={setSelectedMoveObj}/>
+      <PokedexSkillsList details={details} setSelectedMoveObj={setSelectedMoveObj} setSelectedAbilityObj={setSelectedAbilityObj}/>
       <PokedexMoveDescription selectedMoveObj={selectedMoveObj} moveDetails={moveDetails} setMoveDetails={setMoveDetails}/>
       <PokedexImage details={details}/>
       <PokedexMoveSummary details={details} setPopoverClass={setPopoverClass} selectedMoveObj={selectedMoveObj} moveDetails={moveDetails}/>
--- src/components/DetailsPokedexComponents/PokedexSkillsList.js
+++ src/components/DetailsPokedexComponents/PokedexSkillsList.js
@@ -2,11 +2,27 @@ import React from 'react';
 import '../../App.css';
 
 function PokedexSkillsList(props) {
-  const { details, setSelectedMoveObj } = props;
+  const { details, setSelectedMoveObj, setSelectedAbilityObj } = props;
   
   return (
     <div className="pokedex-list">
-    <p>{details.name}'s moves</p>
+      <p>{details.name}'s skills</p>
+      <ul>
+        {details && details.abilities.map((abilityObj, index)=> {
+            const number = String(index + 1).padStart(2, "0");
+
+            return (
+              <li key={index}>
+                <button onClick={() => {
+                  setSelectedAbilityObj(abilityObj);
+                }}>
+                  <strong>{number}.</strong>{abilityObj.ability.name}
+                </button>
+              </li>
+            );
+        })}
+      </ul>
+      <p>{details.name}'s moves</p>
       <ul>
         {details && details.moves.map((moveObj, index)=> {
           const number = String(index + 1).padStart(2, "0");
